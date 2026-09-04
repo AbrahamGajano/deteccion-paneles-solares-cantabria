b7db45042665f66af6a0804e2147033ec8cf518f
AbrahamGajano <abrahamgajano10@gmail.com>
2026-09-04 11:48:54 +0200

    Preparar seleccion de datos para entreno

--- src/preparar_bdappv.py
+++ src/preparar_bdappv.py
@@ -5,6 +5,10 @@ import pandas as pd
 RUTA_IMAGENES = Path("./data/extern/bdappv/ign/img")
 RUTA_MASCARAS = Path("./data/extern/bdappv/ign/mask")
 
+RATIO_TRAIN = 80
+NUM_POS = 4000
+NUM_NEG = 4000
+
 
 def buscar_datos() -> pd.DataFrame:
     """Funcion que busca los datos de las imagenes y sus mascaras
@@ -38,8 +42,58 @@ def buscar_datos() -> pd.DataFrame:
     return df_union
 
 
-def seleccionar_datos(datos: pd.DataFrame, positivos: int, negativos: int):
-    """Selecciona positivos y negativos y los divide en train y val."""
+def seleccionar_datos(
+    datos: pd.DataFrame, positivos: int, negativos: int
+) -> pd.DataFrame:
+    """Funcion que selecciona los datos en las cantidades solicitadas dividiendo
+    entre datos de entreno y validacion
+
+    Args:
+        datos (pd.DataFrame): Dataframe de los datos y con informacion de si son pos
+        positivos (int): Numero de positivos
+        negativos (int): Numero de negativos
+
+    Raises:
+        ValueError: En caso de solicitar mas de los existentes
+
+    Returns:
+        _type_: _description_
+    """
+    # Separamos en positivos y negativos
+    positivos_solicitados = datos[datos["tiene_mascara"]]
+    negativos_solicitados = datos[~datos["tiene_mascara"]]
+
+    # Debemos comprobar el no pasarnos del limite
+    if len(positivos_solicitados) < positivos or len(negativos_solicitados) < negativos:
+        raise ValueError("Se han soliciado mas de los posibles")
+
+    # En cada caso nos quedamos con las cantidades solicitadas
+    df_positivos = positivos_solicitados.sample(n=positivos)
+    df_negativos = negativos_solicitados.sample(n=negativos)
+
+    # Dentro de esa seleccion tenemos que decidir si es un valor de entreno o test
+    num_train_pos = int(len(df_positivos) * RATIO_TRAIN)
+    num_train_neg = int(len(df_negativos) * RATIO_TRAIN)
+
+    # Inicializamos a val y despues simplemente lo sobreescribo a train los aleatorios
+    # IMP: AQUI llamo val a este subgrupo ya que nos servira para ver como de bien
+    # Funciona con el dataset frances no con el del PNOA que nos interesa pero al ser
+    # De una resolucion similar 20 vs 15 y 400*400px vs 512*512px nos queda al
+    # Redimensionar muy parecido pero lo adecuado seria corroborarlo con los test
+    # manuales del labelme
+    df_positivos["grupo"] = "val"
+    df_negativos["grupo"] = "val"
+
+    indx_pos_train = df_positivos.sample(n=num_train_pos).index
+
+    df_positivos.loc[indx_pos_train, "grupo"] = "train"
+
+    indx_neg_train = df_negativos.sample(n=num_train_neg).index
+
+    df_negativos.loc[indx_neg_train, "grupo"] = "train"
+
+    # Debemos reconcatenar
+    return pd.concat([df_negativos, df_positivos], ignore_index=True)
 
 
 def convertir_mascara_yolo(ruta_mascara: Path, ruta_txt: Path) -> None:
